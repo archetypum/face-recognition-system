05e54754753c91b9768542e3203f628c0b17236a
Hakki Riza Kucuk <hakkirizakucuk@yandex.com>
2018-08-22 16:55:23 +0200

    brain logo added.

--- src/Component/Logo/Logo.jsx
+++ src/Component/Logo/Logo.jsx
@@ -2,13 +2,29 @@ import React from "react";
 import Tilt from "react-tilt";
 
 import "./Logo.css"
+import brain from "./brain.png";
 
 const Logo = () => {
     return (
         <div className="ma4 mt0">
-        <Tilt className="Tilt br2 shadow-2" options={{ max : 30 }} style={{ height: 250, width: 250 }} >
-            <div className="Tilt-inner">  </div>
-        </Tilt>
+            <Tilt
+                className="Tilt br2 shadow-2"
+                options={{
+                max: 30
+            }}
+                style={{
+                height: 100,
+                width: 100
+            }}>
+                <div className="Tilt-inner pa3">
+                    <img
+                        src={brain}
+                        alt="brain"
+                        style={{
+                        paddingTop: '5px' 
+                    }}/>
+                </div>
+            </Tilt>
         </div>
     );
 };
