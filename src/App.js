14aa65a4a355638534196519c65adcd383090872
Hakki Riza Kucuk <hakkirizakucuk@yandex.com>
2018-08-22 16:36:10 +0200

    basic styling.

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, {Component} from 'react';
 import './App.css';
 import Navigation from './Component/Navigation/Navigation';
+import 'tachyons';
 class App extends Component {
     render() {
         return (
