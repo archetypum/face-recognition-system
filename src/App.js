f9d187e2193d8739177b731ff0b51a7d27c2fca4
Hakki Riza Kucuk <hakkirizakucuk@yandex.com>
2018-08-23 08:39:37 +0200

    background particles effect added.

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, {Component} from 'react';
 import 'tachyons';
+import Particles from 'react-particles-js';
 
 import './App.css';
 import Navigation from './Component/Navigation/Navigation';
@@ -7,15 +8,27 @@ import Logo from './Component/Logo/Logo';
 import Rank from './Component/Rank/Rank';
 import ImageLinkForm from './Component/ImageLinkForm/ImageLinkForm';
 
+const particleParams = {
+    particles: {
+        number: {
+            value: 30,
+            density: {
+                enable: true,
+                value_area: 300
+            }
+        }
+    }
+}
 
 class App extends Component {
     render() {
         return (
             <div className="App">
+                <Particles params={particleParams} className="particles"/>
                 <Navigation/>
                 <Logo/>
                 <Rank/>
-                <ImageLinkForm/>
+                <ImageLinkForm/> 
                 {/* <FaceRecognition/> */}
             </div>
         );
