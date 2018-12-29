525f3ad8f033429d3302a3db476e514092be479f
hakki riza kucuk <hakkirizakucuk@yandex.com>
2018-12-29 18:30:10 +0100

    Updates

---
+++ precache-manifest.4aa6f8a4c4d00aa332e90a0fdf22101a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "a77840e284f967e441f4",
+    "url": "/face-recognition-system/static/css/main.651ae63e.chunk.css"
+  },
+  {
+    "revision": "a77840e284f967e441f4",
+    "url": "/face-recognition-system/static/js/main.a77840e2.chunk.js"
+  },
+  {
+    "revision": "9be2d83a02ce8db47c4e",
+    "url": "/face-recognition-system/static/css/1.42751205.chunk.css"
+  },
+  {
+    "revision": "9be2d83a02ce8db47c4e",
+    "url": "/face-recognition-system/static/js/1.9be2d83a.chunk.js"
+  },
+  {
+    "revision": "5ecfacc9f7b4adb14ccb",
+    "url": "/face-recognition-system/static/js/runtime~main.5ecfacc9.js"
+  },
+  {
+    "revision": "bba8b11f05dcd77619ef5971cf6fb949",
+    "url": "/face-recognition-system/index.html"
+  }
+];
